590b4a6273c3c540fe469e7ae0c849a30d59ade7
Rebecca Manzi <rebeccamanzi@gmail.com>
2020-12-27 19:29:29 -0300

    finish users CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,57 @@
+const { create } = require('domain');
+const { KnexTimeoutError } = require('knex');
+const { nextTick } = require('process');
+const { update } = require('../database');
 const knex = require('../database');
 
 module.exports = {
+  // lista todos os usuarios
   async index(req, res) {
-    const results = await kenx('users');
-
+    const results = await knex('users');
     return res.json(results);
   },
+
+  // cria usuario
+  async create(req, res, next) {
+    try {
+      const { username } = req.body;
+
+      await knex('users').insert({
+        username,
+      });
+
+      return res.status(201).send();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  // atualiza um usuario
+  async update(req, res, next) {
+    try {
+      const { username } = req.body;
+      const { id } = req.params;
+
+      await knex('users').update({ username }).where({ id });
+
+      return res.send();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  // delete um usuario
+  async delete(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      await knex('users').where({ id }).del();
+
+      return res.send();
+    } catch (error) {
+      next(error);
+    }
+  },
 };
+
+// argumento next -> para capturar o erro, caso dê
--- src/server.js
+++ src/server.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
+
+app.use(express.json()); // irá reconhecer o body como json
+app.use(routes);
+
+// not found
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+// capturar todos os erros (catch all):
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
 
 app.listen(3333, () => console.log('Server is running! ✔'));
